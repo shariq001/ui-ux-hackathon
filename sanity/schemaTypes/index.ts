0b65e9e2e5144b06fb24aad8fa5fb94aad0bd61e
Muhammad Shariq <muhammadshariqfazal@gmail.com>
2025-01-24 11:18:29 +0500

    update hackathon-3

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,9 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { Category } from './category'
-import { product } from './product'
+import { product } from './Product'
+
 
 
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product, Category],
+  types: [Category, product],
 }
